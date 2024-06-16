a0fcd9059a1861a4503e5458417499f0eca92373
Diu-R <d845112005@tmu.edu.tw>
2024-06-16 23:32:55 +0800

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -4,22 +4,32 @@
 
 ## The first function creates a special "matrix" object that can cache its inverse.
 ## It returns a list containing functions to set and get the matrix and its inverse.
+## First two objects are initialized - x and inv
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
+  ## inv is set to NULL, initialized as an object 
+  ## within makeCacheMatrix() environment to be used by later code in the function.
+  inv <- NULL 
+  
+  ## Function to set the matrix value
+   set <- function(y) {
     x <<- y
+    
+    ## Reset inverse cache when matrix changes
     inv <<- NULL
-  }
+   }
+  
+  ## Function to get the matrix value
   get <- function() x
   setinv <- function(inverse) inv <<- inverse
   getinv <- function() inv
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
+  
+  
+  ## assigns each of these functions as an element within a list(), 
+  ## and returns it to the parent environment.
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
-
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix.
 ## If the inverse has already been calculated (and the matrix has not changed),
@@ -28,11 +38,16 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
+  
+  ## if the value here is not equal to NULL, we have a valid, cached inverse and 
+  ## can return it to the parent environment
   if (!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
   data <- x$get()
+  
+  # Only calculate inverse if not already cached
   inv <- solve(data, ...)
   x$setinv(inv)
   inv
